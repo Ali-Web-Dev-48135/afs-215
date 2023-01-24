767aaa68511b4ff948dfe469b2a9cc785af605e6
Ali Al Dairawi <ali.aldairawi@bryanuniversity.edu>
2023-01-23 19:51:43 -0800

    Week 3 Assignment Two Mocha & Chai

---
+++ week3/Assignment_Two/lib/NumberClass.js
@@ -0,0 +1,22 @@
+module.exports = class NumberClass {
+  constructor(value) {
+    this.value = value;
+  }
+
+  testuserinput() {
+    if (this.value % 3 === 0 && this.value % 7 === 0) {
+      return "Good Evening";
+    } else if (this.value % 3 === 0 || this.value === 3) {
+      return "Good Morning";
+    } else if (this.value % 7 === 0 || this.value === 7) {
+      return "Good Afternoon";
+    } else if (this.value % 3 === 0 && this.value % 7 === 0) {
+      return "Good Evening";
+    } else if (this.value % 3 !== 0 || this.value % 7 !== 0) {
+      return String(this.value);
+    } else if (typeof this.value !== 'number') {
+      return "This is a custom error message";
+    }
+  }
+};
+
---
+++ week3/Assignment_Two/test/test.js
@@ -0,0 +1,53 @@
+const { assert } = require("chai");
+
+// Requiring the class created (NumberClass).
+const NumberClass = require("../lib/NumberClass");
+
+// Actual Class Object To Instantiate the Number Class.
+const goodMorningClassObject = new NumberClass(3);
+const goodAfternoonClassObject = new NumberClass(7);
+const goodEveningClassObject = new NumberClass(21);
+const neitherSevenOrThreeClasObject = new NumberClass(5);
+const errorMessageClasObject = new NumberClass("Hello World");
+
+
+describe("Good Morning Returned?", function () {
+  describe("Test Good Morning Returned", function () {
+    it("Should return good morning", function () {
+      assert.equal(goodMorningClassObject.testuserinput(), "Good Morning");
+    });
+  });
+});
+
+describe("Good Afternoon Returned?", function () {
+  describe("Test Good Afternoon Returned", function () {
+    it("Should return good afternoon", function () {
+      assert.equal(goodAfternoonClassObject.testuserinput(), "Good Afternoon");
+    });
+  });
+});
+
+describe("Good Evening Returned?", function () {
+  describe("Test Good Evening Returned", function () {
+    it("Should return good evening", function () {
+      assert.equal(goodEveningClassObject.testuserinput(), "Good Evening");
+    });
+  });
+});
+
+describe("Return Number As String?", function () {
+  describe("Test Number Returned As A String", function () {
+    it("Should return A String", function () {
+      assert.typeOf(neitherSevenOrThreeClasObject.testuserinput(), 'string');
+    });
+  });
+});
+
+describe("Good Afternoon Returned?", function () {
+  describe("Test Good Afternoon Returned", function () {
+    it("Should return good afternoon", function () {
+      assert.equal(goodAfternoonClassObject.testuserinput(), "Good Afternoon");
+    });
+  });
+});
+
